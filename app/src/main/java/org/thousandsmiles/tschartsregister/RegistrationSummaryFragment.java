91cb96e6d2df8b425c071deef3c7989888497059
Syd Logan <slogan621@gmail.com>
2018-04-28 09:08:39 -0700

    remove unused imports in registration summary fragment

--- app/src/main/java/org/thousandsmiles/tschartsregister/RegistrationSummaryFragment.java
+++ app/src/main/java/org/thousandsmiles/tschartsregister/RegistrationSummaryFragment.java
@@ -18,37 +18,18 @@
 package org.thousandsmiles.tschartsregister;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TableRow;
 import android.widget.TextView;
-
 import com.squareup.picasso.MemoryPolicy;
-import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
-
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import static android.app.Activity.RESULT_OK;
 
 public class RegistrationSummaryFragment extends Fragment {
     private Activity m_activity = null;
