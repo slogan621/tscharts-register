aad2a7bfe2711038b912c2c22b5e40ad75f766e9
Syd Logan <slogan621@gmail.com>
2020-10-14 20:47:15 -0700

    Clean up layout of search results table to avoid giant headshots
    When there was only one partial row of search results, they would stretch
    to fill entire row. This change addresses it

--- app/src/main/java/org/thousandsmiles/tschartsregister/PatientSearchActivity.java
+++ app/src/main/java/org/thousandsmiles/tschartsregister/PatientSearchActivity.java
@@ -212,8 +212,6 @@ public class PatientSearchActivity extends AppCompatActivity implements ImageDis
 
         LinearLayout btnLO = new LinearLayout(this);
 
-        LinearLayout.LayoutParams paramsLO = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         btnLO.setOrientation(LinearLayout.VERTICAL);
 
         TableRow.LayoutParams parms = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
@@ -280,16 +278,13 @@ public class PatientSearchActivity extends AppCompatActivity implements ImageDis
         }
 
         if (newRow == true) {
-            layout.addView(row, new TableLayout.LayoutParams(0, TableLayout.LayoutParams.WRAP_CONTENT));
+            layout.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
         }
 
         HashMap<Integer, PatientData> map = m_sess.getPatientHashMap();
 
         count = 1;
         int extraCells = (map.size() + 1) % 3;
-        if (extraCells != 0) {
-            extraCells = 3 - extraCells;
-        }
 
         for (Map.Entry<Integer, PatientData> entry : map.entrySet()) {
             Integer key = entry.getKey();
@@ -355,9 +350,6 @@ public class PatientSearchActivity extends AppCompatActivity implements ImageDis
                 });
             }
 
-            //m_sess.addHeadShotPath(id, headshot.getImageFileAbsolutePath());
-            //t.start();
-
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                 RegisterDialogFragment rtc = new RegisterDialogFragment();
@@ -380,21 +372,32 @@ public class PatientSearchActivity extends AppCompatActivity implements ImageDis
             }
 
             if (newRow == true) {
-                layout.addView(row, new TableLayout.LayoutParams(0, TableLayout.LayoutParams.WRAP_CONTENT));
+                layout.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
             }
             count++;
         }
+
+        // this matters most if there is a single row with less than 3 columns. Add invisible headshots to fill
+        // the space, otherwise, the items in a single row result will expand to fill all available space.
 
         for (int i = 0; i < extraCells; i++) {
             btnLO = new LinearLayout(this);
 
             btnLO.setOrientation(LinearLayout.VERTICAL);
 
+            button = new ImageButton(getApplicationContext());
+
+            button.setImageDrawable(getResources().getDrawable(R.drawable.girlfront));
+
+            button.setVisibility(View.INVISIBLE); // we want it to take space, not be visible
             btnLO.setLayoutParams(parms);
+            btnLO.addView(button);
+
             if (row != null) {
                 row.addView(btnLO);
             }
         }
+
         m_sess.getCommonSessionSingleton().startNextHeadshotJob();
     }
 
