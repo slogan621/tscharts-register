602167723daadafb5d570c59d7efcaed72a82bdd
Syd Logan <slogan621@gmail.com>
2021-07-13 10:45:23 -0700

    fix android studio warning

--- app/src/main/java/org/thousandsmiles/tschartsregister/LoginActivity.java
+++ app/src/main/java/org/thousandsmiles/tschartsregister/LoginActivity.java
@@ -173,6 +173,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_READ_CONTACTS) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 populateAutoComplete();
